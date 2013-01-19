2514cfe2c5796b672a991d1b77a50a4a376a6f0f
Florian Maunier <fmaunier@gmail.com>
2013-01-19 20:20:57 +0100

    Ajout d'un filtre médian relâché

--- traitementImage.c
+++ traitementImage.c
@@ -1,20 +1,18 @@
 #include <stdlib.h>
 #include "BmpGris.h"
 
-static int compareChars (void const *a, void const *b)
+int compareChars (const void *a, const void *b)
     {
-       /* definir des pointeurs type's et initialise's
-          avec les parametres */
-       int const *pa = a;
-       int const *pb = b;
-
-       /* evaluer et retourner l'etat de l'evaluation (tri croissant) */
-       return *pa - *pb;
+            const unsigned char *ia = (const unsigned char *)a; // casting pointer types 
+            const unsigned char *ib = (const unsigned char *)b;
+            return (int)(*ia  - *ib); 
     }
 
 DonneesImageGris *filtreMedianRelache(DonneesImageGris *image)
 {
-    int i, j;
+    int i, j, k;
+    int borneBas = 4, borneHaut = 6;
+    bool valeurTrouvee = false;
     
     DonneesImageGris *newImage = (DonneesImageGris*)calloc(1, sizeof(DonneesImageGris));
     newImage->largeurImage = image->largeurImage;
@@ -23,21 +21,34 @@ DonneesImageGris *filtreMedianRelache(DonneesImageGris *image)
 	char **newDonneesGris = inittableau2dchar(image->largeurImage, image->hauteurImage);
     newImage->donneesGris = (unsigned char **)newDonneesGris;
 	
-    for( i = 0; i < image->hauteurImage; i++)
-    for( j = 0; j < image->largeurImage; j++)
+    for( i = 1; i < image->hauteurImage-1; i++)
+    for( j = 1; j < image->largeurImage-1; j++)
 	{
-		char region[9] = {image->donneesGris[i-1][j-1],
-						  image->donneesGris[i][j-1],
-					      image->donneesGris[i+1][j-1],
-						  image->donneesGris[i-1][j],
-						  image->donneesGris[i][j],
-                          image->donneesGris[i+1][j],
-                          image->donneesGris[i-1][j+1],
-                          image->donneesGris[i][j+1],
-                          image->donneesGris[i+1][j+1]
+		char region[9] = {image->donneesGris[j-1][i-1],
+						  image->donneesGris[j-1][i],
+                          image->donneesGris[j-1][i+1],
+						  image->donneesGris[j][i-1],
+						  image->donneesGris[j][i],
+                          image->donneesGris[j][i+1],
+					      image->donneesGris[j+1][i-1],
+                          image->donneesGris[j+1][i],
+                          image->donneesGris[j+1][i+1]
 						 };
         qsort(region, sizeof region / sizeof *region, sizeof *region, compareChars);
-        newDonneesGris[j][i] = region[5];
+        
+        for(k = borneBas; k <= borneHaut; k++)
+        {
+            if(region[k] == image->donneesGris[j][i])
+            {
+                newDonneesGris[j][i] = image->donneesGris[j][i];
+                valeurTrouvee = true;
+            }
+        }
+        
+        if(!valeurTrouvee)
+            newDonneesGris[j][i] = region[5];
+        
+        valeurTrouvee = false;
 	}
     
     return newImage;
